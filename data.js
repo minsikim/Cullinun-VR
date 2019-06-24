744a4e0085cbcca8b71a9705ed14ecce7b6203eb
Salinger MS <minsiua@gmail.com>
2019-06-24 17:02:49 +0900

    190624 data update

---
+++ data.js
@@ -0,0 +1,178 @@
+var DATA = {
+    "25A-1": [
+        {
+            "data": "25A-1_1",
+            "name": "거실",
+            "pointX": 40,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_2",
+            "name": "주방",
+            "pointX": 35,
+            "pointY": 75,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_3",
+            "name": "마스터 베드룸",
+            "pointX": 80,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_4",
+            "name": "마스터 화장실",
+            "pointX": 75,
+            "pointY": 80,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_5",
+            "name": "화장실1",
+            "pointX": 75,
+            "pointY": 75,
+            "startYaw": 0
+        },
+                {
+            "data": "25A-1_6",
+            "name": "외부 정원",
+            "pointX": 50,
+            "pointY": 40,
+            "startYaw": 0
+        },
+    ],
+    "25A-2": [
+        {
+            "data": "25A-1_1",
+            "name": "거실",
+            "pointX": 40,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_2",
+            "name": "주방",
+            "pointX": 35,
+            "pointY": 75,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_3",
+            "name": "마스터 베드룸",
+            "pointX": 80,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_4",
+            "name": "마스터 화장실",
+            "pointX": 75,
+            "pointY": 80,
+            "startYaw": 0
+        },
+        {
+            "data": "25A-1_5",
+            "name": "화장실1",
+            "pointX": 75,
+            "pointY": 75,
+            "startYaw": 0
+        },
+                {
+            "data": "25A-1_6",
+            "name": "외부 정원",
+            "pointX": 50,
+            "pointY": 40,
+            "startYaw": 0
+        },
+    ],
+    "33B-1": [
+        {
+            "data": "33B-1_1",
+            "name": "거실",
+            "pointX": 40,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-1_2",
+            "name": "주방",
+            "pointX": 35,
+            "pointY": 75,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-1_3",
+            "name": "마스터 베드룸",
+            "pointX": 80,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-1_4",
+            "name": "마스터 화장실",
+            "pointX": 75,
+            "pointY": 80,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-1_5",
+            "name": "화장실1",
+            "pointX": 75,
+            "pointY": 75,
+            "startYaw": 0
+        },
+                {
+            "data": "33B-1_6",
+            "name": "외부 정원",
+            "pointX": 50,
+            "pointY": 40,
+            "startYaw": 0
+        },
+    ],
+    "25A-1": [
+        {
+            "data": "33B-2_1",
+            "name": "거실",
+            "pointX": 40,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-2_2",
+            "name": "주방",
+            "pointX": 35,
+            "pointY": 75,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-2_3",
+            "name": "마스터 베드룸",
+            "pointX": 80,
+            "pointY": 60,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-2_4",
+            "name": "마스터 화장실",
+            "pointX": 75,
+            "pointY": 80,
+            "startYaw": 0
+        },
+        {
+            "data": "33B-2_5",
+            "name": "화장실1",
+            "pointX": 75,
+            "pointY": 75,
+            "startYaw": 0
+        },
+                {
+            "data": "33B-2_6",
+            "name": "외부 정원",
+            "pointX": 50,
+            "pointY": 40,
+            "startYaw": 0
+        },
+    ],
+}
